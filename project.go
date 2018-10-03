52c93b05c3f7f0034961c53eeff9b6a3ceb14313
Aleksandr Arofikin <sashaaro@gmail.com>
2018-10-03 20:18:59 +0300

    get network aliases

--- project.go
+++ project.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"context"
 	"fmt"
 	"github.com/docker/docker/api/types"
@@ -11,7 +9,6 @@ import (
 	"github.com/google/tcpproxy"
 	"log"
 	"net"
-	"net/http"
 	"strings"
 	"time"
 )
@@ -34,14 +31,22 @@ func (this *ContainerProxy) createClient () {
 
 }
 
-
 func (this *ContainerProxy) loadContainers () {
 	fmt.Println("loadContainers")
 	containers, err := this.cli.ContainerList(context.Background(), types.ContainerListOptions{})
 	if err != nil {
 		fmt.Printf(err.Error());
 	} else {
+		// fmt.Println("aliases %s", len(containers[0].NetworkSettings.Networks["my_network"].Aliases))
+
 		this.containers = containers
+
+		// get aliases
+		for _, container := range this.containers {
+			con, _ := this.cli.ContainerInspect(context.Background(), container.ID)
+			container.NetworkSettings.Networks["my_network"].Aliases =
+			con.NetworkSettings.Networks["my_network"].Aliases
+		}
 	}
 }
 
@@ -79,16 +84,12 @@ func (ch *ContainerByAliasesTarget) HandleConn(conn net.Conn)  {
 	if wrap.HostName == "" {
 		return;
 	}
-	// fmt.Printf("Host: %v \n", wrap.HostName)
-	//dp := &tcpproxy.DialProxy{Addr: "172.19.0.2:80"}
-	//dp.HandleConn(conn)
-	//return
 
 	for _, container := range ch.containerProxy.containers {
-		for n, network := range container.NetworkSettings.Networks {
-			fmt.Printf("Network: %v", n)
+		for _, network := range container.NetworkSettings.Networks {
+			/*fmt.Printf("Network: %v", n)
 			fmt.Printf("IPAddress: %v", network.IPAddress)
-			fmt.Printf("Aliases: %v", network.Aliases)
+			fmt.Printf("Aliases: %v", network.Aliases)*/
 			for _, alias := range network.Aliases {
 				fmt.Printf("Alias: %s \n", alias)
 				if wrap.HostName == alias {
@@ -139,76 +140,4 @@ func main() {
 		containerProxy.listen()
 	}()
 	containerProxy.start()
-}
-
-
-
-// httpHostHeader returns the HTTP Host header from br without
-// consuming any of its bytes. It returns "" if it can't find one.
-func httpHostHeader(br *bufio.Reader) string {
-	const maxPeek = 4 << 10
-	peekSize := 0
-	for {
-		peekSize++
-		if peekSize > maxPeek {
-			b, _ := br.Peek(br.Buffered())
-			return httpHostHeaderFromBytes(b)
-		}
-		b, err := br.Peek(peekSize)
-		if n := br.Buffered(); n > peekSize {
-			b, _ = br.Peek(n)
-			peekSize = n
-		}
-		if len(b) > 0 {
-			if b[0] < 'A' || b[0] > 'Z' {
-				// Doesn't look like an HTTP verb
-				// (GET, POST, etc).
-				return ""
-			}
-			if bytes.Index(b, crlfcrlf) != -1 || bytes.Index(b, lflf) != -1 {
-				req, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(b)))
-				if err != nil {
-					return ""
-				}
-				if len(req.Header["Host"]) > 1 {
-					// TODO(bradfitz): what does
-					// ReadRequest do if there are
-					// multiple Host headers?
-					return ""
-				}
-				return req.Host
-			}
-		}
-		if err != nil {
-			return httpHostHeaderFromBytes(b)
-		}
-	}
-}
-
-var (
-	lfHostColon = []byte("\nHost:")
-	lfhostColon = []byte("\nhost:")
-	crlf        = []byte("\r\n")
-	lf          = []byte("\n")
-	crlfcrlf    = []byte("\r\n\r\n")
-	lflf        = []byte("\n\n")
-)
-
-func httpHostHeaderFromBytes(b []byte) string {
-	if i := bytes.Index(b, lfHostColon); i != -1 {
-		return string(bytes.TrimSpace(untilEOL(b[i+len(lfHostColon):])))
-	}
-	if i := bytes.Index(b, lfhostColon); i != -1 {
-		return string(bytes.TrimSpace(untilEOL(b[i+len(lfhostColon):])))
-	}
-	return ""
-}
-
-// untilEOL returns v, truncated before the first '\n' byte, if any.
-// The returned slice may include a '\r' at the end.
-func untilEOL(v []byte) []byte {
-	if i := bytes.IndexByte(v, '\n'); i != -1 {
-		return v[:i]
-	}
-	return v
 }
